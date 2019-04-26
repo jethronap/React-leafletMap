403115e69f13920909000d291e0d2f6c59c8401d
jethronap <efthioa@gmail.com>
2019-04-26 13:49:50 +0300

    faked an api, fetched data & upadated state

--- src/App.js
+++ src/App.js
@@ -6,17 +6,27 @@ import Place from './components/place'
 
 class App extends React.Component {
 
-  render() {
-    const place = {
-      "id": 148,
-      "name": "The Maunsell Sea Forts, England",
-      "imageUrl": "https://static.boredpanda.com/blog/wp-content/uuuploads/abandoned-places/abandoned-places-4.jpg",
-      "date": 1950,
-      "lat": 51.51276111,
-      "lng": 1.08916667
+  constructor(props) {
+    super(props);
+    this.state = {
+      places: []
     };
+  }
 
-    const places = [place, place, place];
+  componentDidMount() {
+    // fake url to mock an api:
+    const endpoint = "https://raw.githubusercontent.com/jethronap/ReactEg/master/places.json";
+
+    fetch(endpoint) // AJAX request
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          places: data
+        });
+      })
+  }
+
+  render() {
 
     return (
 
@@ -26,7 +36,7 @@ class App extends React.Component {
           </div>
           {/* use map to iterate through the places array */}
           <div className="places">
-            {places.map((place) => {
+            {this.state.places.map((place) => {
               return <Place place={place} />
             })}
           </div>
