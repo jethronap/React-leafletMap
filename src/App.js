c92d65b9c9bc7500b668fa10d84794472bb9951f
jethronap <efthioa@gmail.com>
2019-04-28 15:55:57 +0300

    added search-bar which filters results

--- src/App.js
+++ src/App.js
@@ -9,7 +9,10 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      places: [],
+      places: [], // this is the filter
+      allPlaces: [], // this never changes
+      selectedPlace: null,
+      search: "",
     };
   }
 
@@ -22,7 +25,7 @@ class App extends React.Component {
       .then((data) => {
         this.setState({
           places: data,
-          selectedPlace: null,
+          allPlaces: data,
         });
       })
   }
@@ -35,7 +38,18 @@ class App extends React.Component {
     })
   }
 
+  handleSearch = (event) => {
+    console.log(event);
+    // change the state when i type sth in search:
+    this.setState({
+      search: event.target.value,
+      // we pass allPlaces here so as to have all places when i backspace or enter nothing in searchbar:
+      places: this.state.allPlaces.filter((place) => new RegExp(event.target.value, "i").exec(place.name))
+    });
+  }
+
   render() {
+
     // we change the center from constant to variable with let:
     let center = [39.98, -28.05];
     let zoom = 2;
@@ -50,39 +64,44 @@ class App extends React.Component {
       <div className="app">
         <div className="main">
           <div className="search">
+            <input
+              type="text"
+              placeholder="Search..."
+              value={this.state.search}
+              onChange={this.handleSearch} />
           </div>
           {/* use map to iterate through the places array */}
           <div className="places" >
             {this.state.places.map((place) => {
-              return <Place 
-              key={place.id} 
-              place={place}
-              selectPlace={this.selectPlace} />
+              return <Place
+                key={place.id}
+                place={place}
+                selectPlace={this.selectPlace} />
             })}
           </div>
-            <LeafletMap className="map"
-              center={center}
-              zoom={zoom}
-              maxZoom={15}
-              attributionControl={true}
-              zoomControl={true}
-              doubleClickZoom={true}
-              scrollWheelZoom={true}
-              dragging={true}
-              animate={true}
-              easeLinearity={0.35}
-            >
-              <TileLayer
-                url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
-              />
-              {this.state.places.map((place) => {
-                return <Marker key={place.id} position={[place.lat, place.lng]}>
-                  <Popup>
-                    hello from {place.name}
-                  </Popup>
-                </Marker>
-              })}
-            </LeafletMap>
+          <LeafletMap className="map"
+            center={center}
+            zoom={zoom}
+            maxZoom={15}
+            attributionControl={true}
+            zoomControl={true}
+            doubleClickZoom={true}
+            scrollWheelZoom={true}
+            dragging={true}
+            animate={true}
+            easeLinearity={0.35}
+          >
+            <TileLayer
+              url='http://{s}.tile.osm.org/{z}/{x}/{y}.png'
+            />
+            {this.state.places.map((place) => {
+              return <Marker key={place.id} position={[place.lat, place.lng]}>
+                <Popup>
+                  hello from {place.name}
+                </Popup>
+              </Marker>
+            })}
+          </LeafletMap>
         </div>
       </div>
 
